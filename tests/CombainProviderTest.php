d0c6a2c30e5efedc1f879ebfb15edb1978957180
Mika Tuupola <tuupola@appelsiini.net>
2017-04-09 19:32:14 +0700

    Add support for Combain CPS API
    https://combain.com/

---
+++ tests/CombainProviderTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of whereami package
+ *
+ * Copyright (c) 2017 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   https://github.com/tuupola/whereami
+ *
+ */
+
+namespace Whereami;
+
+use Http\Discovery\HttpClientDiscovery;
+use Http\Discovery\Strategy\MockClientStrategy;
+use Http\Mock\Client as MockClient;
+use PHPUnit\Framework\TestCase;
+use Whereami\Provider\CombainProvider;
+use Zend\Diactoros\Response;
+use Zend\Diactoros\Stream;
+
+class CombainProviderTest extends TestCase
+{
+    public function setUp()
+    {
+        HttpClientDiscovery::prependStrategy(MockClientStrategy::class);
+    }
+
+    public function testShouldBeTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testShouldConstruct()
+    {
+        $provider = new CombainProvider("fakekey");
+        $this->assertInstanceOf(CombainProvider::class, $provider);
+    }
+
+    public function testShouldProcess()
+    {
+        $stream = new Stream("php://memory", "rb+");
+        $stream->write('{"location":{"lat":1.35985,"lng":103.9608},"accuracy":24}');
+        $response = new Response($stream);
+
+        $mockClient = new MockCLient;
+        $mockClient->addResponse($response);
+        $httpClient = (new HttpClientFactory($mockClient))->create();
+
+        $networks = file_get_contents(__DIR__ . "/changi.json");
+        $networks = json_decode($networks, true);
+
+        $location = (new CombainProvider("fakekey", $httpClient))->process($networks);
+
+        $this->assertEquals(1.35985, $location["latitude"]);
+        $this->assertEquals(103.9608, $location["longitude"]);
+        $this->assertEquals(24, $location["accuracy"]);
+    }
+}
