26c269fd5ea0bc0ca8f5e9215a2460b027c7aa40
Mika Tuupola <tuupola@appelsiini.net>
2017-06-11 14:34:08 +0700

    Use the provided AbstractProvider

--- src/Provider/RadiocellsProvider.php
+++ src/Provider/RadiocellsProvider.php
@@ -15,49 +15,15 @@
 
 namespace Whereami\Provider;
 
-use Http\Client\HttpClient;
-use Http\Discovery\MessageFactoryDiscovery;
-use Http\Message\RequestFactory;
-use Whereami\Exception\BadRequestException;
 use Whereami\Exception\NotFoundException;
-use Whereami\HttpClientFactory;
+use Whereami\Exception\BadRequestException;
 use Whereami\Provider;
 
-final class RadiocellsProvider implements Provider
+final class RadiocellsProvider extends AbstractProvider implements Provider
 {
-    private $endpoint = "https://radiocells.org/backend/geolocate";
+    protected $endpoint = "https://radiocells.org/backend/geolocate";
 
-    private $apikey;
-    private $httpClient;
-    private $requestFactory;
-
-    public function __construct(
-        $apikey,
-        HttpClient $httpClient = null,
-        RequestFactory $requestFactory = null
-    ) {
-        $this->apikey = $apikey;
-        $this->httpClient = $httpClient ?: (new HttpClientFactory)->create();
-        $this->requestFactory = $requestFactory ?: MessageFactoryDiscovery::find();
-    }
-
-    public function process(array $data, array $options = [])
-    {
-        $endpoint = $this->endpoint();
-        $headers = [];
-        $body = $this->transform($data);
-        $request = $this->requestFactory->createRequest("POST", $endpoint, $headers, $body);
-        $response = $this->httpClient->sendRequest($request);
-
-        return $this->parse((string) $response->getBody());
-    }
-
-    private function endpoint()
-    {
-        return $this->endpoint;
-    }
-
-    private function transform($data = [])
+    protected function transform($data = [])
     {
         $json["considerIp"] = false;
         $json["wifiAccessPoints"] = array_map(function ($entry) {
@@ -72,7 +38,7 @@ final class RadiocellsProvider implements Provider
         return json_encode($json, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
     }
 
-    private function parse($json)
+    protected function parse($json)
     {
         $data = json_decode($json, true);
 
